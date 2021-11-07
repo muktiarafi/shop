dc2551ae6f5be2f294d907daea3fabbd8ac7256c
muktiarafi <muktiarafi@gmail.com>
2021-11-07 14:23:22 +0700

    update address dto validation

--- identity/src/main/java/dev/muktiarafi/identity/dto/AddressDto.java
+++ identity/src/main/java/dev/muktiarafi/identity/dto/AddressDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -11,15 +12,19 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddressDto {
+    @NotBlank
     @Size(min = 5)
     private String detail;
 
-    @Size(min = 5)
+    @NotBlank
+    @Pattern(regexp = "\\d{5}", message = "Postal Code is not valid")
     private String postalCode;
 
+    @NotBlank
     @Size(min = 4)
     private String recipientName;
 
-    @Pattern(regexp = "(\\+\\d{2})\\d{10,12}")
+    @NotBlank
+    @Pattern(regexp = "(\\+\\d{2})\\d{10,12}", message = "Phone number is not valid")
     private String recipientPhoneNumber;
 }
